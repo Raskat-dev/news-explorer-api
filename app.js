4e55e34d19709f4a7279d0aa2d05784afd32e317
Raskat <ka-rostislav@yandex.ru>
2020-11-17 15:35:02 +0700

    cors

--- app.js
+++ app.js
@@ -34,24 +34,25 @@ app.use(requestLogger); // подключаем логгер запросов
 app.use(helmet()); // для простановки security-заголовков для API
 app.use(rateLimit); // для ограничения числа запросов
 
-const whitelist = ['https://ne.rk.students.nomoreparties.co', 'http://ne.rk.students.nomoreparties.co', 'https://www.ne.rk.students.nomoreparties.co', 'http://www.ne.rk.students.nomoreparties.co'];
-const corsOptions = {
-  origin(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-};
-app.use(cors(corsOptions));
+// const whitelist = ['https://ne.rk.students.nomoreparties.co', 'http://ne.rk.students.nomoreparties.co', 'https://www.ne.rk.students.nomoreparties.co', 'http://www.ne.rk.students.nomoreparties.co'];
+// const corsOptions = {
+//   origin(origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+// };
+// app.use(cors(corsOptions));
 
-app.options('*', cors({
-  origin: true,
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credential: true,
-  optionsSuccessStatus: 204,
-}));
+// app.options('*', cors({
+//   origin: true,
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   credential: true,
+//   optionsSuccessStatus: 204,
+// }));
+app.use(cors({ origin: true }));
 
 app.use('/', signRouter);
 app.use('/users', auth, userRouter);

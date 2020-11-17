8f46820d7ae9b21f325a973bbeee961b547caaa2
Raskat <ka-rostislav@yandex.ru>
2020-11-17 15:08:19 +0700

    добавил cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const cors = require('cors');
 const errorHandler = require('./middlewares/celebrate/errorHandler'); // обработка ошибок celebrate без вложенности
 const {
   signRouter, userRouter, articleRouter, errorRouter,
@@ -32,6 +33,25 @@ app.use(requestLogger); // подключаем логгер запросов
 
 app.use(helmet()); // для простановки security-заголовков для API
 app.use(rateLimit); // для ограничения числа запросов
+
+const whitelist = ['https://ne.rk.students.nomoreparties.co', 'http://ne.rk.students.nomoreparties.co', 'https://www.ne.rk.students.nomoreparties.co', 'http://www.ne.rk.students.nomoreparties.co'];
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+app.use(cors(corsOptions));
+
+app.options('*', cors({
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credential: true,
+  optionsSuccessStatus: 204,
+}));
 
 app.use('/', signRouter);
 app.use('/users', auth, userRouter);
